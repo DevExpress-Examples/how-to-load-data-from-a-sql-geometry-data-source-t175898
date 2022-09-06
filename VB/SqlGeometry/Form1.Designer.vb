89e4707e1f2a081241c7a45ac0042eed67325ab6
DevExpressExampleBot <fakeEmail>
2022-09-06 12:29:29 +0400

    Source auto update [skip ci]

--- VB/SqlGeometry/Form1.Designer.vb
+++ VB/SqlGeometry/Form1.Designer.vb
@@ -1,59 +1,56 @@
-﻿Namespace SqlGeometry
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace SqlGeometry
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.mapControl1 = New DevExpress.XtraMap.MapControl()
-			CType(Me.mapControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' mapControl1
-			' 
-			Me.mapControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.mapControl1.Location = New System.Drawing.Point(0, 0)
-			Me.mapControl1.Name = "mapControl1"
-			Me.mapControl1.Size = New System.Drawing.Size(784, 561)
-			Me.mapControl1.TabIndex = 0
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(784, 561)
-			Me.Controls.Add(Me.mapControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
-			CType(Me.mapControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.mapControl1 = New DevExpress.XtraMap.MapControl()
+            CType((Me.mapControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' mapControl1
+            ' 
+            Me.mapControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.mapControl1.Location = New System.Drawing.Point(0, 0)
+            Me.mapControl1.Name = "mapControl1"
+            Me.mapControl1.Size = New System.Drawing.Size(784, 561)
+            Me.mapControl1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(784, 561)
+            Me.Controls.Add(Me.mapControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.mapControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private mapControl1 As DevExpress.XtraMap.MapControl
-
-	End Class
+#End Region
+        Private mapControl1 As DevExpress.XtraMap.MapControl
+    End Class
 End Namespace
-
--- VB/SqlGeometry/Form1.vb
+++ VB/SqlGeometry/Form1.vb
@@ -1,57 +1,51 @@
-﻿Imports DevExpress.XtraMap
+Imports DevExpress.XtraMap
 Imports System
 Imports System.Data.SqlClient
 Imports System.IO
 Imports System.Windows.Forms
 
 Namespace SqlGeometry
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits System.Windows.Forms.Form
 
-		Private Const filePath As String = "..\..\Data\SQLG.mdf"
-		Private Shared fullFilePath As String = Path.GetFullPath(Path.Combine(Application.StartupPath, filePath))
-		Private connectionString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=" & fullFilePath & ";Database=SqlGeometryDB;Integrated Security=True;MultipleActiveResultSets=True"
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-			Dim adapter As New SqlGeometryDataAdapter() With {
-				.ConnectionString = connectionString,
-				.SqlText = "SELECT TOP 1000 [id], [GeomCol1],[TextCol] FROM [dbo].[DemoTable]",
-				.SpatialDataMember = "GeomCol1"
-			}
-			Dim layer As New VectorItemsLayer() With {
-				.Data = adapter,
-				.ShapeTitlesPattern = "{TextCol}"
-			}
-			AddHandler layer.DataLoaded, AddressOf layer_DataLoaded
-			mapControl1.Layers.Add(layer)
-			mapControl1.MapEditor.ShowEditorPanel = True
-			AddHandler mapControl1.MapEditor.MapItemEdited, AddressOf MapEditor_MapItemEdited
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub layer_DataLoaded(ByVal sender As Object, ByVal e As DataLoadedEventArgs)
-			mapControl1.ZoomToFitLayerItems()
-		End Sub
+        Const filePath As String = "..\..\Data\SQLG.mdf"
 
-		Private Sub MapEditor_MapItemEdited(ByVal sender As Object, ByVal e As MapItemEditedEventArgs)
-			For Each path As MapPath In e.Items
-				Dim id As Integer = Convert.ToInt32(path.Attributes("id").Value)
-				Dim modified As String = path.ExportToWkt().ToString()
-				path.Attributes("TextCol").Value = "Australia" & " " & DateTime.Now.Second.ToString()
+        Private Shared fullFilePath As String = System.IO.Path.GetFullPath(System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath, SqlGeometry.Form1.filePath))
 
-				Using cn As New SqlConnection() With {.ConnectionString = connectionString}
-					cn.Open()
-					' For more information about SRID parameters, see https://docs.microsoft.com/en-us/sql/t-sql/spatial-geography/stsrid-geography-data-type?view=sql-server-ver15
-					Dim updatecmd As New SqlCommand("UPDATE DemoTable SET GeomCol1 = geometry::STGeomFromText('" & modified & "', 4326 ) WHERE id = " & id.ToString(), cn)
-					updatecmd.ExecuteNonQuery()
+        Private connectionString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=" & SqlGeometry.Form1.fullFilePath & ";Database=SqlGeometryDB;Integrated Security=True;MultipleActiveResultSets=True"
 
-					Dim updateattr As New SqlCommand("UPDATE DemoTable SET TextCol = '" & path.Attributes("TextCol").Value.ToString() & "' WHERE id =" & id.ToString(), cn)
-					updateattr.ExecuteNonQuery()
-				End Using
-			Next path
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs)
+            Dim adapter As DevExpress.XtraMap.SqlGeometryDataAdapter = New DevExpress.XtraMap.SqlGeometryDataAdapter() With {.ConnectionString = Me.connectionString, .SqlText = "SELECT TOP 1000 [id], [GeomCol1],[TextCol] FROM [dbo].[DemoTable]", .SpatialDataMember = "GeomCol1"}
+            Dim layer As DevExpress.XtraMap.VectorItemsLayer = New DevExpress.XtraMap.VectorItemsLayer() With {.Data = adapter, .ShapeTitlesPattern = "{TextCol}"}
+            AddHandler layer.DataLoaded, AddressOf Me.layer_DataLoaded
+            Me.mapControl1.Layers.Add(layer)
+            Me.mapControl1.MapEditor.ShowEditorPanel = True
+            AddHandler Me.mapControl1.MapEditor.MapItemEdited, AddressOf Me.MapEditor_MapItemEdited
+        End Sub
 
-	End Class
+        Private Sub layer_DataLoaded(ByVal sender As Object, ByVal e As DevExpress.XtraMap.DataLoadedEventArgs)
+            Me.mapControl1.ZoomToFitLayerItems()
+        End Sub
+
+        Private Sub MapEditor_MapItemEdited(ByVal sender As Object, ByVal e As DevExpress.XtraMap.MapItemEditedEventArgs)
+            For Each path As DevExpress.XtraMap.MapPath In e.Items
+                Dim id As Integer = System.Convert.ToInt32(path.Attributes(CStr(("id"))).Value)
+                Dim modified As String = path.ExportToWkt().ToString()
+                path.Attributes(CStr(("TextCol"))).Value = "Australia" & " " & System.DateTime.Now.Second.ToString()
+                Using cn As System.Data.SqlClient.SqlConnection = New System.Data.SqlClient.SqlConnection() With {.ConnectionString = Me.connectionString}
+                    cn.Open()
+                    ' For more information about SRID parameters, see https://docs.microsoft.com/en-us/sql/t-sql/spatial-geography/stsrid-geography-data-type?view=sql-server-ver15
+                    Dim updatecmd As System.Data.SqlClient.SqlCommand = New System.Data.SqlClient.SqlCommand("UPDATE DemoTable SET GeomCol1 = geometry::STGeomFromText('" & modified & "', 4326 ) WHERE id = " & id.ToString(), cn)
+                    updatecmd.ExecuteNonQuery()
+                    Dim updateattr As System.Data.SqlClient.SqlCommand = New System.Data.SqlClient.SqlCommand("UPDATE DemoTable SET TextCol = '" & path.Attributes(CStr(("TextCol"))).Value.ToString() & "' WHERE id =" & id.ToString(), cn)
+                    updateattr.ExecuteNonQuery()
+                End Using
+            Next
+        End Sub
+    End Class
 End Namespace
