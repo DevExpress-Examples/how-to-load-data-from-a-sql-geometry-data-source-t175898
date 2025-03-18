35ab71d79154b44afb200f0883b1b5d9288ed5ca
DevExpressExampleBot <fakeEmail>
2025-03-18 22:24:38 +0400

    Source auto update [skip ci]

--- VB/SqlGeometry/Form1.Designer.vb
+++ VB/SqlGeometry/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace SqlGeometry
-    Partial Public Class Form1
+Namespace SqlGeometry
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,21 +12,21 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.mapControl1 = New DevExpress.XtraMap.MapControl()
-            DirectCast(Me.mapControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.mapControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' mapControl1
@@ -43,15 +45,12 @@
             Me.Controls.Add(Me.mapControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.mapControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.mapControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+#End Region
         Private mapControl1 As DevExpress.XtraMap.MapControl
-
     End Class
 End Namespace
-
